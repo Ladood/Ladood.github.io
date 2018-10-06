2d550c4a1dbcb02b896bc72731bbc578c183ca1d
Ladood <dawoodtafsir@gmail.com>
2018-10-06 17:28:33 -0500

    Portfolio

---
+++ Main.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Dawood's Portfolio</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+	<body>
+<center>
+	<h1> Dawood's Portfolio </h1><br>
+</center>
+		<hr width="90%">
+
+	</body>
+
+<!-- Muz Start -->
+<center>
+<body>
+		<a href="images/1.jpg" target="blank">
+	<div class="muz">
+  		<img src="images/1.jpg" alt="Muz" class="image" width="400px">
+    <div class="text">Click To Enlarge</div>
+	</div>
+		</a>
+</body>
+<!-- Muz End -->
+
+<!-- Sheikh Start -->
+
+<body>
+		<a href="images/2.jpg" target="blank">
+	<div class="sheikh">
+  		<img src="images/2.jpg" alt="Sheikh" class="image" width="400px">
+    <div class="text2">Click To Enlarge</div>
+	</div>
+		</a>
+</body>
+<!-- Sheikh End -->
+
+<!-- Hamzah Starts -->
+<body>
+		<a href="images/3.jpg" target="blank">
+	<div class="munshi">
+		<img src="images/3.jpg" alt="Munshi" class="image" width="400px">
+	<div class="text3">Click To Enlarge</div>
+	</div>
+		</a>
+</body>
+<!-- Hamzah Ends -->
+
+<!-- Hummaiz Starts -->
+
+<body>
+		<a href="images/4.jpg" target="blank">
+	<div class="hum">
+		<img src="images/4.jpg" alt="Hummaiz" class="image" width="400px">
+	<div class="text4">Click To Enlarge</div>
+</div>
+</a>
+
+		<a href="images/5.jpg" target="blank">
+	<div class="hum1">
+		<img src="images/5.jpg" alt="Hummaiz1" class="image" width="400px">
+	<div class="text5">Click To Enlarge</div>
+</div>
+</a>
+</body>
+
+<body>
+	<a href="images/6.jpg" target="blank">
+		<div class="ham">
+			<img src="images/6.jpg" alt="Hamna" width="400px" class="image">
+		<div class="text6">Click To Enlarge</div>
+	</div>
+</a>
+</body>
+</center>
+
+
+</html>
